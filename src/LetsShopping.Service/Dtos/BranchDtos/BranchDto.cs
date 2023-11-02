b54acb7aa0d0991a741e82aa30fad17c5891ff2d
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-02 18:27:18 +0500

    ModelsDto

---
+++ src/LetsShopping.Service/Dtos/BranchDtos/BranchDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsShopping.Service.Dtos.BranchDtos
+{
+    public class BranchDto
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string TelePhone { get; set; }
+    }
+}
