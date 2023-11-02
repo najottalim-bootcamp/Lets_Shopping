559ce69ec5fb0475f88111fe0f6d4716897c2dee
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-02 19:19:52 +0500

    Order Interface Added

--- src/LetsShopping.Service/Interfaces/Orders/IOrderDetailsService.cs
+++ src/LetsShopping.Service/Interfaces/Orders/IOrderDetailsService.cs
@@ -1,6 +1,19 @@
-﻿namespace LetsShopping.Service.Interfaces.Orders
+﻿using LetsShopping.Domain.Models.Orders;
+using LetsShopping.Service.Dtos.OrdersDtos;
+
+namespace LetsShopping.Service.Interfaces.Orders
 {
     public interface IOrderDetailsService
     {
+        //Create
+        Task<bool> CreateOrderDetailsAsync(OrderDetailsDto orderDetailsDto);
+        //Delete
+        Task<bool> DeleteOrderDetailsByIdAsync(int orderId);
+        //Get
+        Task<IEnumerable<OrderDetails>> GetAllOrdersAsync(string tableName);
+        //GetById
+        Task<OrderDetails> GetOrderDetailsByIdAsync(int orderId);
+        //Update
+        Task<bool> UpdateOrderDetailsAsync(int orderId,OrderDetailsDto orderDetailsDto);
     }
 }
--- src/LetsShopping.Service/Interfaces/Orders/IOrderItemsService.cs
+++ src/LetsShopping.Service/Interfaces/Orders/IOrderItemsService.cs
@@ -1,7 +1,19 @@
-﻿namespace LetsShopping.Service.Interfaces.Orders
+﻿using LetsShopping.Domain.Models.Orders;
+using LetsShopping.Service.Dtos.OrdersDtos;
+
+namespace LetsShopping.Service.Interfaces.Orders
 {
     public interface IOrderItemsService
     {
-
+        //Create
+        Task<bool> CreateOrderDetailsAsync(OrderItemsDto orderItemsDto);
+        //Delete
+        Task<bool> DeleteOrderDetailsByIdAsync(int orderItemId);
+        //Get
+        Task<IEnumerable<OrderDetails>> GetAllOrdersAsync(string tableName);
+        //GetById
+        Task<OrderDetails> GetOrderDetailsByIdAsync(int orderItemId);
+        //Update
+        Task<bool> UpdateOrderDetailsAsync(int orderItemId, OrderItemsDto orderDetailsDto);
     }
 }
