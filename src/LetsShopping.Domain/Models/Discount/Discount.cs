6517a304b6bc02c2d5d93b972f6e035822009c70
FarxodbekMadrahimov <farhodbekmadrahimov557@gmail.com>
2023-11-02 17:41:04 +0500

    Branch and Discount model created

---
+++ src/LetsShopping.Domain/Models/Discount/Discount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsShopping.Domain.Models.Discount;
+
+public class Discount
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int DiscountPercent { get; set; }
+    public bool Active {get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set;}
+    public DateTime DeletedAt { get; set; }
+}
