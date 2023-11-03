28b287dad6d48bfca34788653cddbf2b529609f9
Alim Axmedov <muhammadalimaxmedov@gmail.com>
2023-11-03 15:21:03 +0500

    interface committed

--- src/LetsShopping.Service/Interfaces/Users/IUserService.cs
+++ src/LetsShopping.Service/Interfaces/Users/IUserService.cs
@@ -1,7 +1,14 @@
-﻿namespace LetsShopping.Service.Interfaces.Users
+﻿
+
+using LetsShopping.Service.Dtos.UsersDtos;
+
+namespace LetsShopping.Service.Interfaces.Users
 {
     public interface IUserService
     {
-
+        public Task<bool> CreateUserAsync(string UserName);
+        public Task<bool> UpdateUserAsync(UsersDto UserName);
+        public Task<bool> DeleteUserByIdAsync(int UserId);
+        public Task<IEnumerable<Domain.Models.Users.Users>> GetAllAsync();
     }
 }
