956e2d538520ae4f55726f4353b9cc170416c06a
Alim Axmedov <muhammadalimaxmedov@gmail.com>
2023-11-03 15:33:51 +0500

    userr

--- src/LetsShopping.Service/Interfaces/Users/IUserService.cs
+++ src/LetsShopping.Service/Interfaces/Users/IUserService.cs
@@ -2,11 +2,11 @@
 {
     public interface IUserService
     {
+
         public ValueTask<bool> CreateUserAsync(string UserName);
         public ValueTask<bool> UpdateUserAsync(UsersDto UserName);
         public ValueTask<bool> DeleteUserByIdAsync(int UserId);
         public ValueTask<List<User>> GetAllAsync(string Tablename);
         public ValueTask<User> GetByIdUser(int UserId, string Tablename);
-
     }
 }
