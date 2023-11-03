9771a6a43916f1f7adda03a7fc23ec2cd96649de
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-03 15:35:07 +0500

    Services

---
+++ src/LetsShopping.Service/Services/Order/OrderItemsServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsShopping.Service.Services.Order
+{
+    internal class OrderItemsServices
+    {
+    }
+}
