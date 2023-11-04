ec249c1d11a2fdce90825cd223a46d95fabbd0f5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-04 15:54:08 +0500

    bjda

--- src/LetsShopping.Service/Interfaces/Users/IUserService.cs
+++ src/LetsShopping.Service/Interfaces/Users/IUserService.cs
@@ -2,6 +2,11 @@
 {
     public interface IUserService
     {
+        public ValueTask<int> CreateUserAsync(UsersDto usersDto);
+        public ValueTask<int> UpdateUserAsync(int UserId,UsersDto UserName);
+        public ValueTask<int> DeleteUserByIdAsync(int UserId);
+        public ValueTask<List<User>> GetAllAsync(string Tablename);
+        public ValueTask<User> GetByIdUser(int UserId, string Tablename);
 
     }
 }
--- src/LetsShopping.Service/Services/User/UserServices.cs
+++ src/LetsShopping.Service/Services/User/UserServices.cs
@@ -2,6 +2,29 @@
 {
     public class UserServices : IUserService
     {
+        public ValueTask<int> CreateUserAsync(UsersDto usersDto)
+        {
+            throw new NotImplementedException();
+        }
 
+        public ValueTask<int> DeleteUserByIdAsync(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<List<Domain.Models.Users.User>> GetAllAsync(string Tablename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Domain.Models.Users.User> GetByIdUser(int UserId, string Tablename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> UpdateUserAsync(int UserId, UsersDto UserName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
