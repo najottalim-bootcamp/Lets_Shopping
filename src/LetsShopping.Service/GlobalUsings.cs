baeb72906a4bcf02eafc0aa1389e363446f83ac6
Alim Axmedov <muhammadalimaxmedov@gmail.com>
2023-11-04 16:14:45 +0500

    Model va Dto file larga Category, Discount, Address lar qo'shildi

--- src/LetsShopping.Service/GlobalUsings.cs
+++ src/LetsShopping.Service/GlobalUsings.cs
@@ -1,11 +1,14 @@
 ﻿global using LetsShopping.Domain.Enums;
 global using LetsShopping.Domain.Models.Orders;
 global using LetsShopping.Domain.Models.Products;
+global using LetsShopping.Domain.Models.Users;
 global using LetsShopping.Service.Dtos.BranchDtos;
 global using LetsShopping.Service.Dtos.OrdersDtos;
 global using LetsShopping.Service.Dtos.ProductsDtos;
+global using LetsShopping.Service.Dtos.UsersDtos;
+global using LetsShopping.Service.Dtos.DiscountDtos;
 global using LetsShopping.Service.Interfaces;
 global using LetsShopping.Service.Interfaces.Products;
 global using LetsShopping.Service.Interfaces.Users;
-global using LetsShopping.Service.Dtos.DiscountDtos;
 global using LetsShopping.Service.Interfaces.Orders;
+
--- src/LetsShopping.Service/Interfaces/GlobalUsing.cs
+++
@@ -1,2 +0,0 @@
-﻿global using LetsShopping.Domain.Models.Users;
-global using LetsShopping.Service.Dtos.UsersDtos;
