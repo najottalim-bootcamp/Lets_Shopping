16749648fbbd8c3d68e1644e5c69e5c9a711e8c4
Alim Axmedov <muhammadalimaxmedov@gmail.com>
2023-11-05 17:09:22 +0500

    obbo

--- src/LetsShopping.Service/Dtos/DiscountDtos/DiscountDto.cs
+++ src/LetsShopping.Service/Dtos/DiscountDtos/DiscountDto.cs
@@ -2,10 +2,13 @@
 {
     public class DiscountDto
     {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public decimal Percents { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DiscountPercent { get; set; }
+        public bool Active { get; set; }
+        public int Percents { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
 }
+
