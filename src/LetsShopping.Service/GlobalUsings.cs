b5b8ffa4f39968233566cbebb789ce3a54fa2d4d
FarxodbekMadrahimov <farhodbekmadrahimov557@gmail.com>
2023-11-05 12:55:25 +0500

    hj  m

--- src/LetsShopping.Service/GlobalUsings.cs
+++ src/LetsShopping.Service/GlobalUsings.cs
@@ -1,6 +1,6 @@
-﻿global using LetsShopping.Domain.Models.Orders;
-global using LetsShopping.Domain.Models.Products;
+﻿global using LetsShopping.Domain.Models.Products;
 global using LetsShopping.Service.Dtos.DiscountDtos;
 global using LetsShopping.Service.Dtos.ProductsDtos;
+global using LetsShopping.Service.Interfaces.Discount;
 global using LetsShopping.Service.Interfaces.Products;
 global using LetsShopping.Service.Interfaces.Users;
--- src/LetsShopping.Service/Services/Discount/DiscountServices.cs
+++ src/LetsShopping.Service/Services/Discount/DiscountServices.cs
@@ -1,30 +1,8 @@
-﻿using LetsShopping.Service.Interfaces;
-
-namespace LetsShopping.Service.Services.Discount
+﻿namespace LetsShopping.Service.Services.Discount
 {
     public class DiscountServices : IDiscountService
     {
-        public ValueTask<bool> CreateDiscount(DiscountDto discount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ValueTask DeleteDiscount(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ValueTask<List<Domain.Models.Discount.Discount>> GetDiscountAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ValueTask<Domain.Models.Discount.Discount> GetDiscountById(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ValueTask<bool> UpdateDiscount(int Id, DiscountDto discount)
+        public Task CreateDiscount(DiscountDto discount)
         {
             throw new NotImplementedException();
         }
