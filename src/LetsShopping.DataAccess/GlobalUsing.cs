28feacaf3dec6669e286d47cf6b730980674f485
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-06 00:41:35 +0500

    UserController Added

--- src/LetsShopping.DataAccess/GlobalUsing.cs
+++ src/LetsShopping.DataAccess/GlobalUsing.cs
@@ -1,14 +1,10 @@
-﻿global using System.Data.SqlClient;
-global using LetsShopping.Domain.Models.Orders;
-global using LetsShopping.Service.Dtos.OrdersDtos;
-global using LetsShopping.Service.Dtos.Price;
-global using Dapper;
+﻿global using Dapper;
 global using LetsShopping.Domain.Dtos.AddressDtos;
-global using LetsShopping.Domain.Models.Addresses;
+global using LetsShopping.Domain.Dtos.CartsDtos;
 global using LetsShopping.Domain.Enums;
 global using LetsShopping.Domain.Models.Addresses;
-global using LetsShopping.Domain.Dtos.CartsDtos;
 global using LetsShopping.Domain.Models.Carts;
+global using LetsShopping.Domain.Models.Orders;
 global using LetsShopping.Domain.Models.Price;
-global using LetsShopping.Service.Dtos.DiscountDtos;
+global using System.Data.SqlClient;
 
