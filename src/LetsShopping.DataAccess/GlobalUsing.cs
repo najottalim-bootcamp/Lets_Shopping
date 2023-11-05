65e4097d9db5487a521f562b783e79cb582db33b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-05 16:40:28 +0500

    .

--- src/LetsShopping.DataAccess/GlobalUsing.cs
+++ src/LetsShopping.DataAccess/GlobalUsing.cs
@@ -1,9 +1,9 @@
 ﻿global using System.Data.SqlClient;
 global using LetsShopping.Domain.Models.Orders;
 global using LetsShopping.Service.Dtos.OrdersDtos;
-global using LetsShopping.Domain.Models.Price;
 global using LetsShopping.Service.Dtos.Price;
 global using Dapper;
 global using LetsShopping.Domain.Dtos.AddressDtos;
 global using LetsShopping.Domain.Models.Addresses;
 global using LetsShopping.Domain.Enums;
+global using LetsShopping.Domain.Models.Price;
