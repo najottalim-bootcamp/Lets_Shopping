cf76a44e1c526d09daf09e42a5b8e1e39e7ecb98
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-06 02:43:45 +0500

    a

--- src/LetsShopping.Service/Interfaces/Users/IUserService.cs
+++ src/LetsShopping.Service/Interfaces/Users/IUserService.cs
@@ -1,38 +1,10 @@
 
-
-namespace LetsShopping.Service.Interfaces.Users
-
-using LetsShopping.DataAccess.Repositories.UserRepository;
+using global::LetsShopping.DataAccess.Repositories.UserRepository;
 
 namespace LetsShopping.Service.Interfaces.Users
 {
     public interface IUserService : IUserRepository
     {
-        public ValueTask<int> CreateUserAsync(UsersDto model);
-        public ValueTask<int> UpdateUserAsync(int Id, UsersDto model);
-        public ValueTask<int> DeleteUserAsync(int Id);
-        public ValueTask<User> GetUserByIdAsync(int Id);
-        public ValueTask<IList<User>> GetAllUserAsync();
-
-
-        public ValueTask<int> CreateCartAsync(CartDto model);
-        public ValueTask<int> UpdateCartAsync(int Id, CartDto model);
-        public ValueTask<int> DeleteCartAsync(int Id);
-        public ValueTask<Cart> GetCartByIdAsync(int Id);
-        public ValueTask<IList<Cart>> GetAllCartAsync();
-
-
-        public ValueTask<int> CreateCardAsync(CardDto model);
-        public ValueTask<int> UpdateCardAsync(int Id, CardDto model);
-        public ValueTask<int> DeleteCardAsync(int Id);
-        public ValueTask<Card> GetCardByIdAsync(int Id);
-        public ValueTask<IList<Card>> GetAllCardAsync();
-
-
-        public ValueTask<IList<Product>> GetAllProduct();
-        public ValueTask<IList<Category>> GetAllCategory();
-        public ValueTask<IList<Domain.Models.Companies.Company>> GetAllCompany();
-        public ValueTask<IList<Domain.Models.Orders.Order>> GetAllOrderByUserId();
 
     }
 }
