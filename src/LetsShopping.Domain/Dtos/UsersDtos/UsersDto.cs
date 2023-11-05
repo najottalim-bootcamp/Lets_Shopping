3b6672f4485a91f65c552c3f74b1d3eec7a6fbb5
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-05 16:07:23 +0500

    fixed

--- src/LetsShopping.Domain/Dtos/UsersDtos/UsersDto.cs
+++ src/LetsShopping.Domain/Dtos/UsersDtos/UsersDto.cs
@@ -1,12 +1,11 @@
-﻿namespace LetsShopping.Domain.Models.Users
+﻿namespace LetsShopping.Domain.Dtos.UsersDtos
 {
-    public class User : BaseClass
+    public class UsersDto
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
-        public DateTime DeletedAt { get; set; }
         public int? CardId { get; set; }
     }
 }
