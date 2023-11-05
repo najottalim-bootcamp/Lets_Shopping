86dc825c38abdef1f0f9f46c971a364b3d68f3ce
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-05 21:09:30 +0500

    BaseServicesInterfaces and ICompanySerfices

--- src/LetsShopping.Service/Interfaces/Company/ICompanyServices.cs
+++ src/LetsShopping.Service/Interfaces/Company/ICompanyServices.cs
@@ -1,8 +1,22 @@
 ﻿
 
+using LetsShopping.Domain.Dtos.CardDtos;
+using LetsShopping.Domain.Dtos.Category;
+using LetsShopping.Domain.Models.Addresses;
+using LetsShopping.Domain.Models.Cards;
+using LetsShopping.Domain.Models.Category;
+using LetsShopping.Domain.Models.Companies;
+using LetsShopping.Domain.Models.Products;
+
 namespace LetsShopping.Service.Interfaces.Company
 {
-    public interface ICompanyServices : IBaseInter<Domain.Models.Companies.Company, CompanyDto>
+    public interface ICompanyServices :IBaseServicesInterfaces<Domain.Models.Companies.Company, CompanyDto> ,
+        IBaseServicesInterfaces<Product,ProductDto>,
+        IBaseServicesInterfaces<Address,AddressDto>,
+        IBaseServicesInterfaces<Card,CardDto>,
+        IBaseServicesInterfaces<Category,CategoryDto>,
+        IBaseServicesInterfaces<Discount,DiscountDto>
     {
+
     }
 }
