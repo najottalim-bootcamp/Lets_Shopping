1ed2012b02bacfbce0dd749dc334942f66405da3
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-05 11:30:31 +0500

    CompanyAndAddress

---
+++ src/LetsShopping.Domain/Dtos/CompnayDtos/CompanyDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsShopping.Domain.Dtos.CompnayDtos
+{
+    public class CompanyDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string СardId { get; set; }
+    }
+}
