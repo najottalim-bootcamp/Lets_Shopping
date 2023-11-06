7335c391fb04abe0078f0e01a587be80956bacc4
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-06 15:09:59 +0500

    UserServicesRegion

--- src/LetsShopping.Service/Services/Users/UserService.cs
+++ src/LetsShopping.Service/Services/Users/UserService.cs
@@ -1,4 +1,5 @@
-﻿using LetsShopping.DataAccess.Repositories.UserRepository;
+﻿using LetsShopping.DataAccess.Repositories.CardsRepositories;
+using LetsShopping.DataAccess.Repositories.UserRepository;
 using LetsShopping.Domain.Dtos.UsersDtos;
 using LetsShopping.Domain.Exceptions.Users;
 using LetsShopping.Domain.Models.Users;
@@ -7,59 +8,119 @@ namespace LetsShopping.Service.Services.Users
 {
     public class UserService : IUserService
     {
-        private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
+
+        private readonly UserRepository _userRepository;
+        private readonly CardRepository _cardRepository;
+
+        #region Card Services
+
+        public ValueTask<int> CreateCardAsync(CardDto model)
         {
-            _userRepository = userRepository;
-        }
-        public async ValueTask<int> CreateAsync(UsersDto model)
-        {
-            int res = await _userRepository.CreateAsync(model);
-            if (res == 0)
-            {
-                throw new UserNotFoundException();
-            }
-            return res;
+            throw new NotImplementedException();
         }
 
-        public async ValueTask<int> DeleteAsync(int Id)
+        public ValueTask<int> DeleteCardAsync(int Id)
         {
-            int res = await _userRepository.DeleteAsync(Id);
-            if (res == 0)
-            {
-                throw new UserNotFoundException();
-            }
-            return res;
+            throw new NotImplementedException();
+        }
+        public ValueTask<IList<Card>> GetAllCardAsync()
+        {
+            throw new NotImplementedException();
         }
 
-        public async ValueTask<IList<User>> GetAllAsync()
+        public ValueTask<int> UpdateCardAsync(int Id, CardDto model)
         {
-            IList<User> users = (await _userRepository.GetAllAsync());
-            if (users == null)
-            {
-                throw new UserNotFoundException();
-            }
-            return users;
+            throw new NotImplementedException();
+        }
+        public ValueTask<Card> GetCardByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
         }
 
-        public async ValueTask<User> GetByIdAsync(int Id)
+        #endregion  Card Services
+
+
+
+        #region Cart Services
+        public ValueTask<int> DeleteCartAsync(int Id)
         {
-            User user = await _userRepository.GetByIdAsync(Id);
-            if (user == null)
-            {
-                throw new UserNotFoundException();
-            }
-            return user;
+            throw new NotImplementedException();
+        }
+        public ValueTask<int> CreateCartAsync(CartDto model)
+        {
+            throw new NotImplementedException();
+        }
+        public ValueTask<IList<Cart>> GetAllCartAsync()
+        {
+            throw new NotImplementedException();
         }
 
-        public async ValueTask<int> UpdateAsync(int Id, UsersDto model)
+        public ValueTask<int> UpdateCartAsync(int Id, CartDto model)
         {
-            int update = await _userRepository.UpdateAsync(Id, model);
-            if (update == 0)
-            {
-                throw new UserNotFoundException();
-            }
-            return update;
+            throw new NotImplementedException();
         }
+
+        public ValueTask<Cart> GetCartByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion Cart Services
+
+
+
+        #region User Services
+        public ValueTask<int> CreateUserAsync(UsersDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> DeleteUserAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+        public ValueTask<IList<User>> GetAllUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public ValueTask<User> GetUserByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public ValueTask<int> UpdateUserAsync(int Id, UsersDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        public ValueTask<IList<Category>> GetAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IList<Domain.Models.Companies.Company>> GetAllCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IList<Domain.Models.Orders.Order>> GetAllOrderByUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IList<Product>> GetAllProduct()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
