5bd38cb80a0f7b1a797ffb88b0e6be8d7d384b8e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-07 12:55:11 +0500

    save

--- src/LetsShopping.Api/Controllers/Users/UsersController.cs
+++ src/LetsShopping.Api/Controllers/Users/UsersController.cs
@@ -10,38 +10,5 @@
         {
             _user = user;
         }
-        [HttpGet]
-        public IActionResult GetAllAsync()
-        {
-
-        var result = _user.GetAllAsync();
-            return Ok(result.Result);
-
-        }
-        [HttpGet("{Id}")]
-        public IActionResult GetByIdAsync(int id)
-        {
-            var result = _user.GetByIdAsync(id);
-            return Ok(result.Result);
-        }
-        [HttpPost]
-        public IActionResult CreateAsync(UsersDto usersDto)
-        {
-            var result = _user.CreateAsync(usersDto);
-            return Ok(result.Result);
-        }
-        [HttpPatch]
-        public IActionResult UpdateAsync(int Id, UsersDto usersDto)
-        {
-            var result = _user.UpdateAsync(Id, usersDto);
-            return Ok(result.Result);
-        }
-        [HttpDelete]
-        public IActionResult DeleteAsync(int Id)
-        {
-            var result = _user.DeleteAsync(Id);
-            return Ok(result.Result);
-        }
-
     }
 }
