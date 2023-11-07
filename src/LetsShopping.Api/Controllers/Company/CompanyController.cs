365c9d2a43cc3d2d4ad530661b32c338258997ce
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-11-07 14:50:36 +0500

    sdwf

--- src/LetsShopping.Api/Controllers/Company/CompanyController.cs
+++ src/LetsShopping.Api/Controllers/Company/CompanyController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LetsShopping.Service.Interfaces.Company;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LetsShopping.Api.Controllers.Company
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class CompanyController : ControllerBase
     {
+        private readonly ICompanyServices _companyServices;
+        public CompanyController(ICompanyServices company)
+        {
+            _companyServices = company;
+        }
+
     }
 }
