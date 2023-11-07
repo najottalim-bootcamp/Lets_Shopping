67e1ddce130742599098291a42a730c5f46ee6d4
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-07 16:19:05 +0500

    fix

--- src/LetsShopping.Api/Controllers/Company/CompanyController.cs
+++ src/LetsShopping.Api/Controllers/Company/CompanyController.cs
@@ -1,4 +1,6 @@
-﻿using LetsShopping.Domain.Exceptions.Companies;
+﻿using LetsShopping.Domain.Dtos.CompnayDtos;
+using LetsShopping.Domain.Exceptions.Companies;
+using LetsShopping.Domain.Models.Companies;
 using LetsShopping.Service.Interfaces.Company;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +23,36 @@ namespace LetsShopping.Api.Controllers.Company
             if (result is not null)
             {
                 return Ok(result);
+            }
+            return BadRequest(new CompanyNotFoundException());
+        }
+        [HttpGet("{Id}")]
+        public async ValueTask<IActionResult> GetCompanyByIdAsync(int id)
+        {
+            var result = await _companyServices.GetCompanyByIdAsync(id);
+            if (result is not null)
+            {
+                return Ok(result);
+            }
+            return BadRequest(new CompanyNotFoundException());
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateCompanyAsync(CompanyDto company)
+        {
+            int result = await _companyServices.CreateCompanyAsync(company);
+            if(result != 0)
+            {
+                return Ok("Added");
+            }
+            return BadRequest("error");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteCompanyByIdAsync(int id)
+        {
+            int result = await _companyServices.DeleteCompanyAsync(id);
+            if (result != 0)
+            {
+                return Ok("deleted");
             }
             return BadRequest(new CompanyNotFoundException());
         }
